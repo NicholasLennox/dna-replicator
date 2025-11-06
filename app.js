5c33b24bf8673708d32700c55cc799394b65ebbe
Nicholas Lennox <nicholas.lennox2010@gmail.com>
2025-11-06 10:17:54 +0100

    added basic population distribution tracking and logging

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-let Replicator = require('./libs/replicator');
-
+const Replicator = require('./libs/replicator');
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
 
     // Create initial replicator
-    const replicatorZero = new Replicator('ATCGAATC', 0.5);
+    const replicatorZero = new Replicator('ATCGAATC', 0.1);
 
     // Create a structure to hold the population
     const population = [];
     population.push(replicatorZero);
 
     // Epochs
-    const NUM_EPOCH = 3;
+    const NUM_EPOCH = 10;
 
     // Delay in printing epoch number
-    const DRAMA_EFFECT = 1000;
+    const DRAMA_EFFECT = 100;
 
     for (let i = 1; i <= NUM_EPOCH; i++) {
 
-        console.log(`Population Epoch: #${i}`);
+        console.log(`Epoch: #${i}`);
 
         /*
             forEach takes a snapshot of the array’s length at the start of the loop and only iterates over those elements.
@@ -33,7 +34,46 @@ async function main() {
         await new Promise(r => setTimeout(r, DRAMA_EFFECT));
     }
 
-    console.log(population.map(r => r.genome));
+    // Count numbers of different replicators (based on genome)
+    /*
+        Need a key-value pair (genmone, count)
+        Go through each element:
+            Key: replicator.genome
+            Value: running total, starting at 0
+    */
+    let populationDistribution = new Map();
+    population.forEach(replicator => {
+        // console.log(populationDistribution.get(replicator.genome));
+        // console.log(isNaN(populationDistribution.get(replicator.genome)));
+        populationDistribution.set(
+            replicator.genome, 
+            isNaN(populationDistribution.get(replicator.genome)) ? 
+                0 
+                : 
+                populationDistribution.get(replicator.genome) + 1);
+        // console.log(populationDistribution.get(replicator.genome));
+    });
+
+    // Total count
+    let totalReplicators = (Array.from(populationDistribution.values()))
+                            .reduce((a,b) => b + a);
+
+    // console.log(`\nTotal replicators: ${totalReplicators}`);
+    // console.log('\nPopulation distribution\n=======================\n');
+    // console.log(populationDistribution);
+
+    // Save to JSON log
+    const fileName = `replicators_${(new Date()).toISOString().replaceAll(':','-').slice(0, 16)}.json`; // : dont work nice in windows so we need to change it
+    const pathToLogFile = path.join(__dirname, 'logs', fileName);
+    let formattedLog = {
+        date: fileName.split('_')[1],
+        replicas: totalReplicators,
+        distribution: Array.from(populationDistribution, (entry) => ({ genome: entry[0], count: entry[1]})) // Array.from takes a mapping arguement (2nd)
+    };
+    fs.writeFileSync(pathToLogFile, JSON.stringify(formattedLog, null, 2));
+    console.log(formattedLog);
+    
+
 }
 
 main()
